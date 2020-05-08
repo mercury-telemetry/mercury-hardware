e71ad5d415b213d58b24b87adbcfb9ee7aef2230
ab7289 <ab7289@nyu.edu>
2020-05-08 13:50:55 -0400

    removed non-hardware files

--- ag_data/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AgDataConfig(AppConfig):
-    name = "ag_data"
--- mercury/tests/test_urls.py
+++
@@ -1,9 +0,0 @@
-from django.test import SimpleTestCase
-from django.urls import reverse, resolve
-from mercury.views import views
-
-
-class TestUrls(SimpleTestCase):
-    def test_index_url_is_resolved(self):
-        url = reverse("mercury:index")
-        self.assertEquals(resolve(url).func.view_class, views.HomePageView)
