59b90d101e7e571ef93bf6fe5703fc99f7535380
Michael Lally <mikelally801@gmail.com>
2021-03-01 11:02:41 -0500

    changes in testing

--- hardware/SensorPi/sense_pi.py
+++ hardware/SensorPi/sense_pi.py
@@ -8,8 +8,8 @@ from hardware.Utils.utils import (
 # Conditional import for sense hat and emulator
 try:
     if not os.environ.get("EMULATE_SENSE_HAT"):
-        from sense_hat import SenseHat
         print("Real Sense")
+        from sense_hat import SenseHat
     else:
         raise ImportError("Import emulator")
 except ImportError:
