90fed857a6490593be1eb76ac831998c8ca9a732
dandipietrantonio <dd2742@nyu.edu>
2021-03-01 11:21:18 -0500

    update in sensepi

--- hardware/SensorPi/sense_pi.py
+++ hardware/SensorPi/sense_pi.py
@@ -59,7 +59,10 @@ class SensePi:
                 id = self.sensor_ids[key]
                 data["sensor_id"] = id
                 data["values"] = {}
-                data["values"][key] = sensor_data[key]
+                if type(sensor_data[key]) == dict: # more than one field
+                    data["values"] = sensor_data[key]
+                else: # only one field
+                    data["values"][key] = sensor_data[key]
 
             data["date"] = date_str_with_current_timezone()
         return data
